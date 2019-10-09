e604aace3866a5a9e143402ff6f6de366ba091cd
Louis-Charles Carrier <louis-charlescarrier@Louis-Charless-MacBook-Pro.local>
2019-10-09 12:00:31 -0400

    updated

--- api/server.js
+++ api/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-
 const carRouter = require('../cars/cars-router.js');
-
 const server = express();
-
 server.use(express.json());
-
 server.use('/api/cars', carRouter);
 
 module.exports = server;
